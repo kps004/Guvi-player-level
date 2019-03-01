e9209de36c2de15a0427e8117bf1e251b770fd39
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-01 15:41:55 +0530

    Create GPS3_6.C

---
+++ GPS3_6.C
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+ char s[1000],t;
+ int i,k,l,j;
+ gets(s);
+ for(l=0;s[l]!='\0';l++);
+ for(i=0;i<l;i++)
+ {
+     if(s[i]==' ')
+     {
+         for(j=i;j<l;j++)
+         {
+             s[j]=s[j+1];
+         }
+     }
+ }
+ s[l-1]='\0';
+ printf("%s",s);
+
+    return 0;
+}
