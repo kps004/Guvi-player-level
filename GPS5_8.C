dd75a4255d631a135e5ecf662da63c3eac6c4662
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-11 19:46:12 +0530

    Create GPS5_8.C

---
+++ GPS5_8.C
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,j,m,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            
+            if((i%2)!=0)
+            {
+                printf(" %d",i);
+            }
+        }
+    }
+    
+
+    return 0;
+}
