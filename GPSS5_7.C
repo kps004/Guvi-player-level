2c187f772e29469fd3b5d85db5d1257e1d214318
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-11 19:36:45 +0530

    Create GPSS5_7.C

---
+++ GPSS5_7.C
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+
+
+int main()
+{
+ int a,b,c;
+   scanf("%d %d %d",&a,&b,&c);
+   a=a+b+c;
+   if(a<=180)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+    
+
+    return 0;
+}
