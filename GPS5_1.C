311bc937bb8af511021467c48df7fcb4f511684d
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-11 16:26:49 +0530

    Create GPS5_1.C

---
+++ GPS5_1.C
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    scanf("%d %d",&n,&i);
+    if(i*i==n)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    //printf("Hello World");
+    
+
+    return 0;
+}
