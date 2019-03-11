61eb668472dc030ed2d33bb6d7de0b6c439d282c
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-11 19:51:53 +0530

    Create GPS5_9.C

---
+++ GPS5_9.C
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>32496)
+    {
+        printf("LONG");
+    }
+    else{
+        printf("INT");
+    }
+    
+
+    return 0;
+}
