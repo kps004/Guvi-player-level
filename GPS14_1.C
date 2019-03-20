ce216c54f4111e476c3c911a07e83deb56344f86
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-20 19:13:00 +0530

    Create GPS14_1.C

---
+++ GPS14_1.C
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int t,r=0,l=0,n;
+    scanf("%d",&n);
+ while(n)
+ {
+     r=n%10;
+     n=n/10;
+     if(r%2==1)
+     {
+         l=l+r;
+     }
+     
+ }
+if(l%2==0)
+{
+    printf("E");
+}
+else
+{
+    printf("O");
+}
+
+    return 0;
+}
