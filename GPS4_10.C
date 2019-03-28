ea04ab4848c64e9023f42c90f53af9d9227ec7ee
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-28 09:55:32 +0530

    Create GPS4_10.C

---
+++ GPS4_10.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int k,i,c=0,j;
+scanf("%d",&k);
+       for(i=0;i<=k;i++)
+        {
+      for(j=k;j>=0;j--)
+         {
+        if((i*1)+(j*2)==k)
+           {
+                c=c+1;
+                 }
+             }
+       }
+printf("%d",c);
+}
