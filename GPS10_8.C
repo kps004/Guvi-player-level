037c15ed53bd3d351e82c5206c61a8af5ed57883
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-28 10:52:51 +0530

    Update GPS10_8.C

--- GPS10_8.C
+++ GPS10_8.C
@@ -1,30 +1,56 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 #include <stdio.h>
 
 int main()
 {
-  
-  char s[100000],t;
-  int i,j,l,m,k=0;
-  
-  scanf("%[^\n]",s);
-  scanf("\n");
-  scanf("%c",&t);
-  
-  for(l=0;s[l]!='\0';l++);
-  
-  for(i=0;i<=t;i++)
-  {
-      
-      for(j=0;j<=t;j++)
+    char s[10000],t;
+    int i,j,n,l;
+    scanf("%[^\n]",s);
+    scanf("\n");
+    scanf("%c",&t);
+    l=0;
+    while(s[l]!='\0')
+    {
+           l++;        
+    }
+    n=t;
+    
+    
+        for(j=0;j<l;j++)
+        {
+            
+            for(i=0;i<l;i++)
+         {
+            if(s[i]==n)
+            {
+                n--;
+
+                break;
+            }
+         }    
+         if(n<48)
+         {
+             break;
+         }
+    }
+    n++;
+    
+
+    if(n==48)
+   {
+    printf("yes");
+   }
+   else
       {
-          if(s[i]==i)
-          {
-              k++;
-              break;
-          }
+          printf("no");
       }
-  }
-  printf(" %d",k);
+
     return 0;
 }
