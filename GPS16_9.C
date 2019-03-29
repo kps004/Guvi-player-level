d6eb6810796f9192774dac15fdd12e6a8dad8617
kps004 <36299014+kps004@users.noreply.github.com>
2019-03-29 12:26:49 +0530

    Create GPS16_9.C

---
+++ GPS16_9.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+int s[10000];
+int r,l=0,k,n=0,i=1;
+scanf("%d %d",&n,&k);
+n=n*k;
+//n=n*k;
+while(n)
+{
+    r=n%2;
+    s[l]=r;
+    l++;
+    n=n/2;
+
+}
+for(i=0;i<l;i++)
+{
+    if(s[i]==1)
+    {
+        n++;
+    }
+}
+printf("%d",n);
+    return 0;
+}
